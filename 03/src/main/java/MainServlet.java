f585b39672c7898fbe34fdb4b18f19ef25760a82
Baumel Márton <marcibaumel@gmail.com>
2021-09-27 11:02:54 +0200

    Rendezés

---
+++ 03/src/main/java/MainServlet.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name="mainServlet",  urlPatterns="*.ize")
+public class MainServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("hi");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+
+}
---
+++ 03/src/main/java/NotSupportedOperationException.java
@@ -0,0 +1,2 @@
+public class NotSupportedOperationException extends RuntimeException{
+}
