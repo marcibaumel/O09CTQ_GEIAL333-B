44454f83c7f3e04184a0b864064f48abcfc80968
Baumel Márton <marcibaumel@gmail.com>
2021-09-27 10:58:56 +0200

    init (első-második-harmadik gyakorlat)

---
+++ 04/firstApp/src/test/java/hu/me/iit/webalk/firstApp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.firstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
