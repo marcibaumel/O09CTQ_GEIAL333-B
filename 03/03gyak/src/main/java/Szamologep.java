d3c287376c69b80449f8801b25dfe1ed0196677f
Baumel Márton <marcibaumel@gmail.com>
2021-09-27 11:00:23 +0200

    Harmadik gyak anyaga

---
+++ 03/03gyak/src/main/java/Szamologep.java
@@ -0,0 +1,14 @@
+public class Szamologep {
+
+        public Double calculate(Double a, Double b, String operator) {
+            if ("+".equals(operator)) {
+                return a + b;
+            } else if ("-".equals(operator)) {
+                return a - b;
+            } else 	if ("*".equals(operator)) {
+                return a * b;
+            } else 	if ("/".equals(operator)) {
+                return a / b;
+            } else throw new NotSupportedOperationException();
+        }
+}
