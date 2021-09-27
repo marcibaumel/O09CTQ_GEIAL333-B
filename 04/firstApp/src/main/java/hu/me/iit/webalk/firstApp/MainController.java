fa8542ce569d33feed57da81e5e2dc8d894952a8
Baumel Márton <marcibaumel@gmail.com>
2021-09-27 11:25:58 +0200

    response body defination

--- 04/firstApp/src/main/java/hu/me/iit/webalk/firstApp/MainController.java
+++ 04/firstApp/src/main/java/hu/me/iit/webalk/firstApp/MainController.java
@@ -3,11 +3,13 @@ package hu.me.iit.webalk.firstApp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/")
+    @ResponseBody
     private String gyoker(){
         return "Csáó";
     }
