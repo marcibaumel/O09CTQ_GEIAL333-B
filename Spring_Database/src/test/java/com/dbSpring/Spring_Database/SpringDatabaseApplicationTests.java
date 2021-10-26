f1dc8c048e26450e12c7ab9d790891b78ad98d88
Baumel Márton <marcibaumel@gmail.com>
2021-10-26 10:36:17 +0200

    init 1026

---
+++ Spring_Database/src/test/java/com/dbSpring/Spring_Database/SpringDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbSpring.Spring_Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
